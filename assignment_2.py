45975d0f38963add215e8bbc4d4cf2ce6fb6508a
redDevil1UR <syedmojizpayam@gmail.com>
2023-06-04 17:14:46 +0530

    simple calculator

---
+++ assignment_2.py
@@ -0,0 +1,21 @@
+
+N1 = float(input("First: "))
+N2 = float(input("Second: "))
+The_Question = input("what operator do you want to use (-,+,*,/): ")
+while The_Question is not ("-" or "+" or "/" or "*"):
+    The_Question = input("invalid input try something else: ")
+    if "-" or "+" or "*" or "/":
+        break
+if The_Question == "+":
+    print(N1 + N2)
+elif The_Question == "-":
+    print(N1 - N2)
+elif The_Question == "*":
+    print(N1 * N2)
+elif The_Question == "/":
+    print(N1 / N2)
+else:
+    print("invalid input")
+input("thank you for using this product. How do you think we can improve it ? ")
+print("your response was helpful byeee")
+
