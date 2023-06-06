6e838a71f78a5f93d6a47a6c7dd3bbcb97fed14e
redDevil1UR <syedmojizpayam@gmail.com>
2023-06-06 12:07:00 +0530

    improvement to previous assignment code

--- assignment_2.py
+++ assignment_2.py
@@ -1,11 +1,35 @@
+#
+# N1 = float(input("First: "))
+# N2 = float(input("Second: "))
+# The_Question = input("what operator do you want to use (-,+,*,/): ")
+# while The_Question is not ("-" or "+" or "/" or "*"):
+#     The_Question = input("invalid input try something else: ")
+#     if "-" or "+" or "*" or "/":
+#         break
+# if The_Question == "+":
+#     print(N1 + N2)
+# elif The_Question == "-":
+#     print(N1 - N2)
+# elif The_Question == "*":
+#     print(N1 * N2)
+# elif The_Question == "/":
+#     print(N1 / N2)
+# else:
+#     print("invalid input")
+# input("thank you for using this product. How do you think we can improve it ? ")
+# print("your response was helpful byeee")
+
+
+
+
+
+
 
 N1 = float(input("First: "))
-N2 = float(input("Second: "))
 The_Question = input("what operator do you want to use (-,+,*,/): ")
-while The_Question is not ("-" or "+" or "/" or "*"):
+while The_Question not in "-/*+":
     The_Question = input("invalid input try something else: ")
-    if "-" or "+" or "*" or "/":
-        break
+N2 = float(input("Second: "))
 if The_Question == "+":
     print(N1 + N2)
 elif The_Question == "-":
@@ -13,7 +37,10 @@ elif The_Question == "-":
 elif The_Question == "*":
     print(N1 * N2)
 elif The_Question == "/":
-    print(N1 / N2)
+    if N2 == 0:
+        print("invalid input")
+    else:
+        print(N1 / N2)
 else:
     print("invalid input")
 input("thank you for using this product. How do you think we can improve it ? ")
