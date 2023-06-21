08e51dd285432bd3d4ae57f284a343b9dcc32469
redDevil1UR <syedmojizpayam@gmail.com>
2023-06-22 00:35:43 +0530

    working on lists

---
+++ list.py
@@ -0,0 +1,15 @@
+nums = [25,12,8998,77,988]
+
+names = ['abu', 'atta', 'alizia', 'mojiz']
+values = [9.4, 'mojiz',9]
+man = [names, values, nums]
+#
+# step1 = man[0]
+# step2 = step1[2]
+#
+# print(step1)
+# print(step2)
+#
+# print(man[0][3])
+# nums.append(45)
+
