14de4c9ca8bc131859b239fef5aacb9e11713ea2
redDevil1UR <syedmojizpayam@gmail.com>
2023-06-23 17:52:23 +0530

    practice for loops

---
+++ for_loops.py
@@ -0,0 +1,25 @@
+languages = ['Swift', 'python', 'cool', 'man']
+for language in languages:
+    print("you")
+    print("are")
+    print("stupid")
+
+
+cars = ['bmw', 'ford', 'innova']
+cars.sort(reverse=True)
+print(cars)
+
+def myfunc(e):
+    return len(e)
+cars = ['bmw', 'ford', 'innova', "mojiz"]
+
+cars.sort(key=myfunc)
+
+print(cars)
+
+
+p = 'kolala is stupid'
+x = p.split()
+print(x)
+
+
